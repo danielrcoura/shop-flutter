7ef07876da6eaa4a8836439170234467fd9dd8d5
danielrcoura <daniel.coura@ccc.ufcg.edu.br>
2019-08-30 12:01:08 -0300

    Create products overview screen

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+import '../models/product.dart';
+import '../widgets/product_item.dart';
+import '../products.dart';
+
+class ProductsOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts = products;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MyShop'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: loadedProducts.length,
+        itemBuilder: (ctx, i) => ProductItem(
+          loadedProducts[i].id,
+          loadedProducts[i].title,
+          loadedProducts[i].imageUrl,
+        ),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+      ),
+    );
+  }
+}
